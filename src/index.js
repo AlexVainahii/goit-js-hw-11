e9ddd2d11150139a156f6615e87c451ea5714e6d
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-14 17:05:07 +0300

    gallery

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import { createMarkup } from './js/createMarkup';
+import { refs } from './js/refs';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
-async function getPhotos() {
-  const { data } = await axios.get('/?key=30581887-eee3cf06f7a40c5ac298de631', {
-    params: {
-      q: 'yellow+flower',
-      image_type: 'photo',
-      pretty: true,
-    },
-  });
+export class PixabayApi{
+  #params= {params:{
+    key: '30581887-eee3cf06f7a40c5ac298de631',
+    q: 'yellow+flower',
+    orientation: 'horizontal',
+    image_type: 'photo',
+    safesearch: true,
+  }
+  };
+  
+async  getPhotos() {
+  const {data} = await axios.get('',this.#params);
   return data;
 }
-console.log(getPhotos());
+}
+const myGallery= new PixabayApi()
+console.log(myGallery.getPhotos());
+// myGallery.getPhotos().then((
+//   {results})=>{
+//     console.log(results);
+//     const markup=createMarkup(results)
+//     console.log(markup);
+//     refs.list.insertAdjacentHTML('beforeend', markup);
+//   }).catch()
+async function get(){
+  try {
+    const data  =  await myGallery.getPhotos();
+    console.log(data);
+    const markup =  createMarkup(data);
+    console.log(markup);
+    refs.list.insertAdjacentHTML('beforeend', markup);
+    
+  } catch (error) {
+   console.log('Щось пішло не так!', error.message);
+    //clearPage();
+  }}
+  get()
---
+++ src/js/createMarkup.js
@@ -0,0 +1,38 @@
+export function createMarkup(photos) {
+   console.log(photos);
+    return photos.hits
+      .map(({ webformatURL, largeImageURL,tags,likes,views,comments,downloads }) => {
+        return  `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" width="350px" height="250px" data-source="${largeImageURL}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <b>${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <b>${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <b>${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <b>${downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      })
+      .join('');
+  }
+
+  
+    
+    
+  
+
+
+
+
+  
