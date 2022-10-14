00093768c5c8ab27e6d276fc20ff087d64ad8fae
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-15 00:29:18 +0300

    startbug

--- src/index.js
+++ src/index.js
@@ -3,24 +3,25 @@ import Notiflix from 'notiflix';
 import { createMarkup } from './js/createMarkup';
 import { refs } from './js/refs';
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.baseURL = 'http://pixabay.com/api';
 
-export class PixabayApi{
-  #params= {params:{
-    key: '30581887-eee3cf06f7a40c5ac298de631',
-    q: 'yellow+flower',
-    orientation: 'horizontal',
-    image_type: 'photo',
-    safesearch: true,
-  }
+export class PixabayApi {
+  #params = {
+    params: {
+      key: '30581887-eee3cf06f7a40c5ac298de631',
+      q: 'yellow+flower',
+      orientation: 'horizontal',
+      image_type: 'photo',
+      safesearch: true,
+    },
   };
-  
-async  getPhotos() {
-  const {data} = await axios.get('',this.#params);
-  return data;
+
+  async getPhotos() {
+    const { data } = await axios.get('', this.#params);
+    return data;
+  }
 }
-}
-const myGallery= new PixabayApi()
+const myGallery = new PixabayApi();
 console.log(myGallery.getPhotos());
 // myGallery.getPhotos().then((
 //   {results})=>{
@@ -29,16 +30,16 @@ console.log(myGallery.getPhotos());
 //     console.log(markup);
 //     refs.list.insertAdjacentHTML('beforeend', markup);
 //   }).catch()
-async function get(){
+async function get() {
   try {
-    const data  =  await myGallery.getPhotos();
+    const data = await myGallery.getPhotos();
     console.log(data);
-    const markup =  createMarkup(data);
+    const markup = createMarkup(data);
     console.log(markup);
     refs.list.insertAdjacentHTML('beforeend', markup);
-    
   } catch (error) {
-   console.log('Щось пішло не так!', error.message);
+    console.log('Щось пішло не так!', error.message);
     //clearPage();
-  }}
-  get()
+  }
+}
+get();
