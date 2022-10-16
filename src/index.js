60d378cae1227808f97f79d722a39317b9bbd1c2
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-10-16 12:21:24 +0300

    finaly1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createMarkup } from './js/createMarkup';
 import { refs } from './js/refs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { PixabayApi } from './js/pixabay';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const options = {
   root: null,
@@ -14,66 +15,16 @@ const Lightbox = new SimpleLightbox('.gallery a', {
   loadingTimeout: 0,
   showCaptions: true,
 });
-const io = new IntersectionObserver(nextPage, options);
-export class PixabayApi {
-  #params = {
-    params: {
-      key: '30581887-eee3cf06f7a40c5ac298de631',
-      q: '',
-      orientation: 'horizontal',
-      image_type: 'photo',
-      safesearch: true,
-      page: 1,
-      per_page: 40,
-    },
-  };
-  constructor() {
-    this.maxP = 0;
-  }
 
-  async getPhotos() {
-    const { data } = await axios.get('', this.#params);
-    console.log(data);
-    return data;
-  }
-  get search() {
-    return this.#params.params.q;
-  }
-  set search(q) {
-    this.#params.params.q = q;
-  }
-  get maxPages() {
-    return this.maxP;
-  }
-  set maxPages(max) {
-    this.maxP = max;
-  }
-
-  incPage() {
-    this.#params.params.page += 1;
-  }
-  resetPage() {
-    this.#params.params.page = 1;
-  }
-  ShowLoadMore() {
-    console.log(this.#params.params.page, this.maxP);
-    return this.#params.params.page > this.maxP;
-  }
-}
-
-const myGallery = new PixabayApi();
-
-async function get() {
-  refs.btn.addEventListener('click', searchSubmit);
+function clearPage() {
+  refs.list.innerHTML = '';
 }
 async function searchSubmit(event) {
   event.preventDefault();
   clearPage();
-
   const inputSearch = refs.input.value.trim().toLowerCase();
   if (!inputSearch) {
     Notiflix.Notify.failure('Enter data for search!');
-
     return;
   }
   myGallery.search = inputSearch;
@@ -84,13 +35,6 @@ async function searchSubmit(event) {
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     const markup = createMarkup(hits);
     refs.list.insertAdjacentHTML('beforeend', markup);
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
     Lightbox.refresh();
     const target = document.querySelector('.photo-card:last-child');
     io.observe(target);
@@ -104,12 +48,6 @@ async function searchSubmit(event) {
     }
   }
 }
-get();
-
-function clearPage() {
-  refs.list.innerHTML = '';
-}
-
 async function nextPage(entries, observer) {
   entries.forEach(async entry => {
     if (entry.isIntersecting) {
@@ -121,7 +59,6 @@ async function nextPage(entries, observer) {
         );
         return;
       }
-
       observer.unobserve(entry.target);
       try {
         const { hits } = await myGallery.getPhotos();
@@ -140,3 +77,6 @@ async function nextPage(entries, observer) {
     }
   });
 }
+const myGallery = new PixabayApi();
+refs.btn.addEventListener('click', searchSubmit);
+const io = new IntersectionObserver(nextPage, options);
